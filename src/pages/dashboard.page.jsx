9a2fe0557871bce5eec86a8fa67ee877240726bf
Saskya <fernandezsaskya@gmail.com>
2024-05-05 17:21:03 +0200

    creating dashboard page

---
+++ src/pages/dashboard.page.jsx
@@ -0,0 +1,14 @@
+import AllHolidays from "../components/holidays/allHolidays";
+import AllUsers from "../components/users/allUsers";
+
+
+const Dashboard = () => {
+    return (
+        <>
+            <AllUsers />
+            <AllHolidays />
+        </>
+    );
+};
+
+export default Dashboard;
